af3cf96c2654ce6c055aa0f14608813263d53877
smarz007 <92134813+smarz007@users.noreply.github.com>
2022-07-10 18:08:05 -0600

    improved performance by appending to list

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
+import os
 import socket
 import time
-import pandas as pd
-import logging
 from datetime import datetime
-from tqdm import tqdm
-import os
 
-clear = lambda: os.system("cls")
+import pandas as pd
+from tqdm import tqdm
 
 # basic data
+# server info
 server = 'irc.chat.twitch.tv'
 port = 6667
-nickname = 'my_wicked_bot'
+nickname = 'chat_scrapper'
+
 done_scraping = False
-df = pd.DataFrame()
-first_run = True
 done = False
-data = []
 conter = 0
 token, channel, points, csv_name, ready = "", "", "", "", ""
 username, message = "", ""
-sock = socket.socket()
-# get current time
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+users = []
+messages = []
+times = []
 
-# start the logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s — %(message)s',
-                    datefmt='%Y-%m-%d_%H:%M:%S',
-                    handlers=[logging.FileHandler('chat.log', encoding='utf-8')])
+sock = socket.socket()
+now = datetime.now()
+
+
+def clear():
+    os.system("cls")
 
 
 def find_username(resp):
@@ -45,6 +42,14 @@ def find_username(resp):
     return username[1:]
 
 
+def connect():
+    """connect to the server and authenticate"""
+    sock.connect((server, port))
+    sock.send(f"PASS {token}\n".encode('utf-8'))
+    sock.send(f"NICK {nickname}\n".encode('utf-8'))
+    sock.send(f"JOIN {channel}\n".encode('utf-8'))
+
+
 def find_message(resp):
     """ gets the message the person sent"""
     location = resp.find(channel)
@@ -56,28 +61,6 @@ def find_message(resp):
     return trimed_message[2:]
 
 
-def return_data(file):
-    with open(file, 'r', encoding='utf-8') as f:
-        lines = f.read().split('\n\n\n')
-
-        for line in lines:
-            try:
-
-                d = {
-                    'dt': current_time,
-                    'channel': channel,
-                    'username': username,
-                    'message': message
-                }
-
-                data.append(d)
-
-            except Exception:
-                pass
-
-    return pd.DataFrame().from_records(data)
-
-
 # main event loop to get the data
 
 while not done:
@@ -85,29 +68,29 @@ while not done:
     token = input("token: ")
     channel = input("channel: ")
     points = int(input("points: "))
-    csv_name = input("csv name: ")
+    csv_name = input("csv name: ") + ".csv"
     clear()
-    while not ready == "ready":
+    while not ready == "ready" or ready == "r":
 
         print(f"""
 
-             ▄▄·  ▄ .▄ ▄▄▄· ▄▄▄▄▄    ▄▄▄  ▪   ▄▄▄· ▄▄▄·▄▄▄ .▄▄▄  
-            ▐█ ▌▪██▪▐█▐█ ▀█ •██      ▀▄ █·██ ▐█ ▄█▐█ ▄█▀▄.▀·▀▄ █·
-            ██ ▄▄██▀▐█▄█▀▀█  ▐█.▪    ▐▀▀▄ ▐█· ██▀· ██▀·▐▀▀▪▄▐▀▀▄ 
-            ▐███▌██▌▐▀▐█ ▪▐▌ ▐█▌·    ▐█•█▌▐█▌▐█▪·•▐█▪·•▐█▄▄▌▐█•█▌
-            ·▀▀▀ ▀▀▀ · ▀  ▀  ▀▀▀     .▀  ▀▀▀▀.▀   .▀    ▀▀▀ .▀  ▀
-                                                        by smarz
-
-                            1. token: {token}
-                            ----------------------
-                            2. channel: {channel}
-                            ----------------------
-                            3. data points: {points}
-                            ----------------------
-                            4. name of csv:{csv_name}.csv
-                            ----------------------
-                            type "ready" to start
-                            ----------------------      
+                     ▄▄·  ▄ .▄ ▄▄▄· ▄▄▄▄▄    ▄▄▄  ▪   ▄▄▄· ▄▄▄·▄▄▄ .▄▄▄  
+                    ▐█ ▌▪██▪▐█▐█ ▀█ •██      ▀▄ █·██ ▐█ ▄█▐█ ▄█▀▄.▀·▀▄ █·
+                    ██ ▄▄██▀▐█▄█▀▀█  ▐█.▪    ▐▀▀▄ ▐█· ██▀· ██▀·▐▀▀▪▄▐▀▀▄ 
+                    ▐███▌██▌▐▀▐█ ▪▐▌ ▐█▌·    ▐█•█▌▐█▌▐█▪·•▐█▪·•▐█▄▄▌▐█•█▌
+                    ·▀▀▀ ▀▀▀ · ▀  ▀  ▀▀▀     .▀  ▀▀▀▀.▀   .▀    ▀▀▀ .▀  ▀
+                                                                by smarz
+        
+                                    1. token: {token}
+                                    ----------------------
+                                    2. channel: {channel}
+                                    ----------------------
+                                    3. data points: {points}
+                                    ----------------------
+                                    4. name of csv:{csv_name}
+                                    ----------------------
+                                    type "ready" to start
+                                    ----------------------      
 
         """)
 
@@ -129,17 +112,13 @@ while not done:
 
     channel = "#" + channel
     # initialize the socket
-
-    sock.connect((server, port))
-
-    # connect to the server by sending the user and password
-    sock.send(f"PASS {token}\n".encode('utf-8'))
-    sock.send(f"NICK {nickname}\n".encode('utf-8'))
-    sock.send(f"JOIN {channel}\n".encode('utf-8'))
+    connect()
 
     for i in tqdm(range(points + 2)):
         time.sleep(0.1)
+        conter += 1
 
+        # response from twitch chat
         resp = sock.recv(2048).decode("utf-8", "ignore")
 
         # check if the message received is the ping response. if it is reply
@@ -148,31 +127,36 @@ while not done:
 
         username = find_username(resp)
         message = find_message(resp)
+        current_time = now.strftime("%H:%M:%S")
 
-        if username == "" or username is None:
-            print("a connection error has occurred. disconnecting and reconnecting...")
-            sock.shutdown(socket.SHUT_RDWR)
-            sock.close()
-            time.sleep(30)
-            sock.connect((server, port))
-            sock.send(f"PASS {token}\n".encode('utf-8'))
-            sock.send(f"NICK {nickname}\n".encode('utf-8'))
-            sock.send(f"JOIN {channel}\n".encode('utf-8'))
-            time.sleep(1)
+        # if the username is blank a connection error has occured the restart
+        # if username == "" or username is None:
+        #     print("a connection error has occurred. disconnecting and reconnecting...")
+        #     sock.shutdown(socket.SHUT_RDWR)
+        #     sock.close()
+        #     time.sleep(15)
+        #
+        #     connect()
+        #     time.sleep(1)
 
+        # this gets rid of the hello message twitch sends at the start
         if conter > 2:
-            df = return_data("chat.log")
-
+            users.append(username)
+            messages.append(message)
+            times.append(current_time)
+            print(username)
 
     done_scraping = True
 
-
-
-    csv_name += ".csv"
     if done_scraping:
-        df.to_csv(csv_name, encoding='utf-8')
+        time_series = pd.Series(times, name="times")
+        users_series = pd.Series(users, name="username")
+        messages_series = pd.Series(messages, name="message")
+        df = pd.concat([time_series, users_series, messages_series], axis=1)
+
+        df.to_csv(csv_name, encoding="utf-8")
+
         done = True
+sock.shutdown(socket.SHUT_RDWR)
 sock.close()
 print("done")
-
-# add a random message to the front page # gonna kill myself trying to add this
